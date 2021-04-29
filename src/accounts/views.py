8959a27cd31c11a3f939cb5f15a39fb4f7c586f3
AmiraSakr-Intake41 <amira.21sakr@gmail.com>
2021-04-30 00:04:10 +0200

    Remove unused login function

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from .forms import NewUserForm
-from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm #add this
+#from django.contrib import messages
+#from django.contrib.auth.forms import AuthenticationForm #add this
 
 
 # Create your views here.
@@ -22,23 +22,5 @@ def signup(request):
     })
 
 
-def login(request):
-    form = AuthenticationForm(request.POST or None)
-    if form.is_valid():
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password')
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.info(request, f"You are now logged in as {username}.")
-            #return redirect()
-        else:
-            messages.error(request, "Invalid username or password.")
-    else:
-        messages.error(request, "Invalid username or password.")
-
-    return render(request, "registration/login.html", {
-        "form": form
-    })
 
 
