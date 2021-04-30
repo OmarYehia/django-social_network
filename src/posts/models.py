ccf6936d24cac371e5c6c81931f5f24a2fe8a71c
AmiraSakr-Intake41 <amira.21sakr@gmail.com>
2021-04-30 16:44:49 +0200

    Adding some functions to profile model

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,9 +1,13 @@
 from django.db import models
-from django.db import models
+from profiles.models import Profile
 
 
 class Post(models.Model):
     title = models.TextField(max_length=250)
     body = models.TextField(max_length=250)
-    # author
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, related_name='posts')
+    created = created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return str(self.title)
 
--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from itertools import chain
+import random
+
 
 # Create your models here.
 class Profile(models.Model):
@@ -14,3 +17,53 @@ class Profile(models.Model):
     def __str__(self):
         return str(self.user)
 
+    def get_my_posts(self):
+        return self.posts.all()
+
+    @property
+    def num_posts(self):
+        return self.posts.all().count()
+
+    def get_following(self):
+        return self.following.all()
+
+    def get_following_users(self):
+        following_list = [profile for profile in self.get_following()]
+        return following_list
+
+    def get_my_and_following_posts(self):
+        users = [user for user in self.get_following()]
+        posts = []
+        qs = None
+        for u in  users:
+            profile = Profile.objects.get(user=u)
+            profile_posts = profile.posts.all()
+            posts.append(profile_posts)
+        my_posts = self.posts.all()
+        posts.append(my_posts)
+        if len(posts) > 0:
+            qs = sorted(chain(*posts), reverse=True, key=lambda obj: obj.created)
+        return qs
+
+    def get_proposals_for_following(self):
+        profiles = Profile.objects.all().exclude(user=self.user)
+        followers_list = [profile for profile in self.get_following()]
+        available = [profile.user for profile in profiles if profile.user not in followers_list]
+        random.shuffle(available)
+        return available[:3]
+
+    @property
+    def following_count(self):
+        return self.get_following().count()
+
+    def get_followers(self):
+        qs = Profile.objects.all()
+        followers_list = []
+        for profile in qs:
+            if self.user in profile.get_following():
+                followers_list.append(profile)
+        return followers_list
+
+    @property
+    def followers_count(self):
+         return len(self.get_followers())
