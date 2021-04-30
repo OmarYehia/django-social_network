773eb4ca235a516d20e94ca8caf7e3f05e558b7f
AmiraSakr-Intake41 <amira.21sakr@gmail.com>
2021-04-30 02:18:30 +0200

    Modifying html files

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("signup", views.signup, name="signup"),
+    path("profile_view", views.profile_view, name="profile-view"),
 
 ]
