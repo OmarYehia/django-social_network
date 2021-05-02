53dc2c3a0f0ebe79fd07aa81dc297068f0a78946
AmiraSakr-Intake41 <amira.21sakr@gmail.com>
2021-05-02 14:11:40 +0200

    my_profile page is modified to let user see profiles to follow and put login_required restrictions on this page

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("profile-view", views.profile_view, name="profile-view"),
-    path('my/', views.MyProfileView.as_view(), name='my-profile-view'),
+    path('my/', views.my_profile_view, name='my-profile-view'),
     path('my-profile-json/', views.MyProfileData.as_view(), name='my-profile-json')
 
 ]
