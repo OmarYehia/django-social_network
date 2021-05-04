69ce8a5375f9fe2068e43988a3366eff7a9eb9eb
Omar Yehia <oyehia94@gmail.com>
2021-05-04 16:40:37 +0200

    Fix error to unathenticated users

--- src/social_network/views.py
+++ src/social_network/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 
 
 def index(request):
-    if not request.user:
+    if not request.user.is_authenticated:
         return render(request, 'main/index.html', {})
 
     return redirect("posts:posts-index")
