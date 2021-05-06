8b1ca0978d6ccaec30f94e15e5ae0f9702fe3a00
Omar Yehia <oyehia94@gmail.com>
2021-05-06 14:16:46 +0200

    Fixing merge issues

--- src/social_network/settings.py
+++ src/social_network/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'social_network',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'djangouser',
+        'PASSWORD': 'Qwer1234*',
         'HOST': 'localhost'
     }
 }
